1b839462fc252870b505c77015a32d4d07f57b15
rkb <rumbletumjum@gmail.com>
2022-04-08 20:34:20 -0400

    initial commit; basic skeleton

---
+++ babywm.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <X11/Xlib.h>
+
+
+int main(int argc, char *argv[])
+{
+    Display *display;
+    int screen;
+    Window root;
+
+    printf("opening display\n");
+    if ((display = XOpenDisplay(NULL)) == NULL) {
+        printf("error opening display\n");
+        exit(1);
+    }
+
+    screen = DefaultScreen(display);
+    root = RootWindow(display, screen);
+
+    return 0;
+}
